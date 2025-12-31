3b961b3d399cb87a80ae9c5e861876e15fc24333
huaji2369 <3117086599@qq.com>
2025-12-31 18:40:20 +0800

    switch to gentoo

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,4 @@
 if status is-interactive
+	zoxide init fish --cmd=cd | source
 end
-set -gx fish_complete_path /home/huaji2369/.config/fish/completions /etc/fish/completions /home/huaji2369/.local/share/fish/vendor_completions.d /home/huaji2369/.cache/fish/generated_completions /usr/share/fish/vendor_completions.d/
-set -gx fish_function_path /home/huaji2369/.config/fish/functions /etc/fish/functions /home/huaji2369/.local/share/fish/vendor_functions.d /usr/share/fish/vendor_functions.d/
-zoxide init --cmd cd fish | source
+set -Ux EIX_LIMIT 0
